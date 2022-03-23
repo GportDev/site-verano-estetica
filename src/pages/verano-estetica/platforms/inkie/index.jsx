8bb51a229ab9de214223512c39a60a467efcba53
GportDev <gbporteiro@gmail.com>
2022-03-23 15:53:25 -0300

    Error fixing 02

--- src/pages/verano-estetica/platforms/inkie/index.jsx
+++ src/pages/verano-estetica/platforms/inkie/index.jsx
@@ -5,8 +5,8 @@ import Footer from "../../../../components/footer";
 import { ButtonFilled, Container, ContentSection, Subtitle, Text  } from "../../../../components/styles/styles";
 import { ListLine, PlatSection, BenefitsList, Equipment, ThirdTitle, ImgDiv, Picture } from "../../styles";
 import InkiePic from "../../../../assets/images/ND_YAG/inkie.png"
-import Tratamento1 from "../../../../assets/images/ND YAG/tatoo.jpeg"
-import Tratamento2 from "../../../../assets/images/ND YAG/sobrancelha.jpeg"
+import Tratamento1 from "../../../../assets/images/ND_YAG/tatoo.jpeg"
+import Tratamento2 from "../../../../assets/images/ND_YAG/sobrancelha.jpeg"
 
 
 function Inkie() {
